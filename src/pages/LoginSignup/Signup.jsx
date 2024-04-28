6e84159952f32899547b737c83233a36ab7c5141
Anirban Dutta <duttashaan102@gmail.com>
2024-04-28 12:30:49 +0530

    fixed signup page ft@doyelsd123

--- src/pages/LoginSignup/Signup.jsx
+++ src/pages/LoginSignup/Signup.jsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import React, { act, useState } from 'react'
-=======
 import {React, useState} from 'react'
->>>>>>> 9066a758efe6e5085adb173c6b845bcb18151ef8
-import './Signup.css'
 import {addUser} from "../../service/api"
+import './Signup.css'
 
 import user_icon from '../assets/person.png'
 import email_icon from '../assets/email.png'
@@ -19,9 +15,6 @@ const initialValues={
   
 }
 const Signup = () => {
-<<<<<<< HEAD
-  const [action, setAction] = useState("Sign Up");
-=======
   const [user,setUser]=useState(initialValues)
   const onValueChange=(e)=>{
     setUser({...user,[e.target.id]:e.target.value})
@@ -32,33 +25,18 @@ const addUserDetails=async()=>{
   await addUser(user);
 
 }
->>>>>>> 9066a758efe6e5085adb173c6b845bcb18151ef8
   return (
     <div className="sign">
-    
+    <div className='linkPage'>
+        <div className="link">sign up</div>
+        <div className="link">login</div>
+    </div>
     <div className='container'>
-      <div className="header">
-        <div className="text">{action}</div>
+    <div className="header">
+        <div className="text">Sign up</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-<<<<<<< HEAD
-        {action==="Login"?<div></div>:<div className="input">
-            <img src={user_icon} alt="Name"/>
-            <input type="text" placeholder='Name' id='name'/>
-        </div>}
-        
-        <div className="input">
-            <img src={email_icon} alt="" />
-            <input type="email" placeholder='Email Id' id='email'/>
-        </div>
-        {action==="Login"?<div></div>:<div className="input">
-            <img src={email_icon} alt="" />
-            <input type="number" placeholder='Phone no' id='phno'/>
-        </div>}
-        
-        <div className="input">
-=======
         <formcontrol class="input">
             <img src={user_icon} alt="Name"/>
             <input type="text" onChange={(e)=> onValueChange(e)} placeholder='Name' id='name'/>
@@ -77,7 +55,6 @@ const addUserDetails=async()=>{
             <input type="text" onChange={(e)=> onValueChange(e)} placeholder='username' id='username'/>
         </formcontrol >
         <formcontrol class="input">
->>>>>>> 9066a758efe6e5085adb173c6b845bcb18151ef8
             <img src={password_icon} alt="" />
             <input type="password" placeholder='Password' id='password' autoComplete='current-password' onChange={(e)=> onValueChange(e)}/>
         </formcontrol>
@@ -85,15 +62,10 @@ const addUserDetails=async()=>{
           <button className="submit" onClick={()=>addUserDetails()}>Sign Up</button>
         </formcontrol>
       </div>
-      {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost password? <span>Click here</span></div>}
+      {/* {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost password? <span>Click here</span></div>} */}
       
       <div className="submit-container">
-<<<<<<< HEAD
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Signup</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-=======
         {/* <input className='submit' type='submit' placeholder='Submit'></input> */}
->>>>>>> 9066a758efe6e5085adb173c6b845bcb18151ef8
       </div>
     </div>
   )
