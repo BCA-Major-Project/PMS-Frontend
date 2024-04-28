ca96b206606994b48bbb5a64350ec5df112578e3
Anirban Dutta <duttashaan102@gmail.com>
2024-04-28 12:56:11 +0530

    Fix signup styling ft.@doyelsd123

--- src/pages/LoginSignup/Signup.jsx
+++ src/pages/LoginSignup/Signup.jsx
@@ -26,46 +26,39 @@ const addUserDetails=async()=>{
 
 }
   return (
-    <div className="sign">
-    <div className='linkPage'>
-        <div className="link">sign up</div>
-        <div className="link">login</div>
-    </div>
+    <div className="signup">
     <div className='container'>
-    <div className="header">
+      <div className="header">
         <div className="text">Sign up</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <formcontrol class="input">
-            <img src={user_icon} alt="Name"/>
-            <input type="text" onChange={(e)=> onValueChange(e)} placeholder='Name' id='name'/>
-        </formcontrol>
-        </div>
-        <formcontrol class="input">
-            <img src={email_icon} alt="" />
-            <input type="email" onChange={(e)=> onValueChange(e)} placeholder='Email Id' id='email'/>
-        </formcontrol>
-        <formcontrol class="input">
-            <img src={email_icon} alt="" />
-            <input type="number" onChange={(e)=> onValueChange(e)} placeholder='Phone no' id='phno'/>
-        </formcontrol >
-        <formcontrol class="input">
-            <img src={email_icon} alt="" />
-            <input type="text" onChange={(e)=> onValueChange(e)} placeholder='username' id='username'/>
-        </formcontrol >
-        <formcontrol class="input">
-            <img src={password_icon} alt="" />
-            <input type="password" placeholder='Password' id='password' autoComplete='current-password' onChange={(e)=> onValueChange(e)}/>
-        </formcontrol>
-        <formcontrol>
-          <button className="submit" onClick={()=>addUserDetails()}>Sign Up</button>
-        </formcontrol>
+          <formcontrol class="input">
+              <img src={user_icon} alt="Name"/>
+              <input type="text" onChange={(e)=> onValueChange(e)} placeholder='Name' id='name'/>
+          </formcontrol>
+          
+          <formcontrol class="input">
+              <img src={email_icon} alt="" />
+              <input type="email" onChange={(e)=> onValueChange(e)} placeholder='Email Id' id='email'/>
+          </formcontrol>
+          <formcontrol class="input">
+              <img src={email_icon} alt="" />
+              <input type="number" onChange={(e)=> onValueChange(e)} placeholder='Phone no' id='phno'/>
+          </formcontrol >
+          <formcontrol class="input">
+              <img src={email_icon} alt="" />
+              <input type="text" onChange={(e)=> onValueChange(e)} placeholder='username' id='username'/>
+          </formcontrol >
+          <formcontrol class="input">
+              <img src={password_icon} alt="" />
+              <input type="password" placeholder='Password' id='password' autoComplete='current-password' onChange={(e)=> onValueChange(e)}/>
+          </formcontrol>
       </div>
-      {/* {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost password? <span>Click here</span></div>} */}
-      
-      <div className="submit-container">
-        {/* <input className='submit' type='submit' placeholder='Submit'></input> */}
+          <formcontrol>
+            <button className="submit" onClick={()=>addUserDetails()}>Sign Up</button>
+          </formcontrol>
+          <div className="forgot-password">Already got an account? <span>Login</span></div>
       </div>
     </div>
   )
