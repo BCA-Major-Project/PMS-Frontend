7bfa9bba7fd4143d292803ced0df364e26777cc0
baishali <roybaishali07@gmail.com>
2024-04-29 20:31:05 +0530

    added edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/LoginSignup/Login';
 import Forgotpwd from './pages/LoginSignup/Forgotpwd';
 import Userdetails from './pages/Admin/Userdetails';
 import AdminHome from './pages/Admin/AdminHome';
+import EditUser from './pages/Admin/EditUser';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/userdetails" element={<Userdetails/>} />
           <Route path="/adminlogin" element={<Adminlogin/>} />
           <Route path="/adminhome" element={<AdminHome/>} />
+          <Route path="/edit/:id" element={<EditUser/>} />
       </Routes>
     </BrowserRouter>
   );
