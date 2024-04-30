efb1c5314bc5945485df579646d843c8fe7db625
baishali <roybaishali07@gmail.com>
2024-04-30 11:01:53 +0530

    added new EditUser css

--- src/pages/Admin/EditUser.jsx
+++ src/pages/Admin/EditUser.jsx
@@ -1,73 +1,105 @@
-import styled from "@emotion/styled";
-import { useState ,useEffect} from "react";
-import { FormControl, FormGroup, InputLabel, Input, TextField, Typography ,Button} from "@mui/material";
-import { getUser,editUser } from "../../service/api";
-import { Navigate, useParams } from "react-router-dom";
-const Container= styled(FormGroup)`
-width:50%;
-margin:5% auto 0 auto;
-&>div{
-    margin-top:20px
-}`
-const initialValues={
-    name: '',
-    email: '',
-    phno: '',
-    password: ''
-}
-const EditUser=()=>{
-    const [user,setUser]=useState(initialValues)
-    const {id}=useParams();
+import { useState, useEffect } from "react";
+import { FormControl, InputLabel, Input, TextField, Typography, Button, Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { getUser, editUser } from "../../service/api";
+import { useParams } from "react-router-dom";
 
-    useEffect(()=>{
-        getUserData();
-    },[])
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center", 
+    alignItems: "center", 
+    minHeight: "100vh", 
+  },
+  formContainer: {
+    width: "50%",
+    padding: "30px",
+    backgroundColor: "#f0f0f0",
+    borderRadius: "10px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  },
+  inputLabel: {
+    marginBottom: "10px",
+  },
+  submitButton: {
+    marginTop: "20px",
+    backgroundColor: "#4caf50",
+    borderRadius: "20px",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#388e3c",
+    },
+  },
+}));
 
-    const getUserData=async()=>{
-        let response=await getUser(id);
-        console.log(response);
-       setUser(response.data);
+const initialValues = {
+  name: "",
+  email: "",
+  phno: "",
+  password: "",
+};
 
-    }
+const EditUser = () => {
+  const classes = useStyles();
+  const [user, setUser] = useState(initialValues);
+  const { id } = useParams();
 
-    const onValueChange=(e)=>{
-       
-        setUser({...user,[e.target.name]:e.target.value})
-        console.log(user)
-    }
+  useEffect(() => {
+    getUserData();
+  }, []);
 
-    const addUserDetails=async()=>{
-      await editUser(user,id);
-      //Navigate('view');
+  const getUserData = async () => {
+    let response = await getUser(id);
+    setUser(response.data);
+  };
 
-    }
-    return(
-        //<p>Hello Register</p>
-        <Container>
-           
-            <Typography variant="h3">Edit User</Typography>
-            <FormControl>
-                <InputLabel>Name</InputLabel>
-               <Input onChange={(e)=> onValueChange(e)}name="name" value={user.name}/>
-            </FormControl>
-            <FormControl>
-                <InputLabel>Email </InputLabel>
-                <Input onChange={(e)=> onValueChange(e)}name="email" value={user.email}/>
-            </FormControl>
-            <FormControl>
-                <InputLabel>Phno</InputLabel>
-                <Input onChange={(e)=> onValueChange(e)}name="phno" value={user.phno}/>
-            </FormControl>
-            <FormControl>
-            <TextField id="outlined-password-input"
-             label="Password" 
+  const onValueChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const addUserDetails = async () => {
+    await editUser(user, id);
+    //Navigate('view');
+  };
+
+  return (
+    <div className={classes.container}>
+      <Box className={classes.formContainer}>
+        <Typography variant="h3" align="center" gutterBottom>
+          EDIT USER
+        </Typography>
+        <FormControl fullWidth margin="normal">
+          <InputLabel className={classes.inputLabel}>Name</InputLabel>
+          <Input onChange={(e) => onValueChange(e)} name="name" value={user.name} />
+        </FormControl>
+        <FormControl fullWidth margin="normal">
+          <InputLabel className={classes.inputLabel}>Email</InputLabel>
+          <Input onChange={(e) => onValueChange(e)} name="email" value={user.email} />
+        </FormControl>
+        <FormControl fullWidth margin="normal">
+          <InputLabel className={classes.inputLabel}>Phno</InputLabel>
+          <Input onChange={(e) => onValueChange(e)} name="phno" value={user.phno} />
+        </FormControl>
+        <FormControl fullWidth margin="normal">
+          <TextField
+            id="outlined-password-input"
+            label="Password"
             type="password"
-            autoComplete="current-password" onChange={(e)=> onValueChange(e)}name="password" value={user.password}/>
-         </FormControl>
-         <FormControl>
-         <Button onClick={()=>addUserDetails()}variant="contained">EditUser</Button>
-         </FormControl>
-        </Container>
-    )
-}
+            autoComplete="current-password"
+            onChange={(e) => onValueChange(e)}
+            name="password"
+            value={user.password}
+          />
+        </FormControl>
+        <FormControl fullWidth margin="normal">
+          <Button onClick={addUserDetails} variant="contained" className={classes.submitButton} size="large" fullWidth>
+            Edit User
+          </Button>
+        </FormControl>
+      </Box>
+    </div>
+  );
+};
+
 export default EditUser;
