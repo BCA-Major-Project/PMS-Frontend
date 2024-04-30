307e26177a0f286c3c481564bd2ae93c29c48625
baishali <roybaishali07@gmail.com>
2024-04-30 11:42:15 +0530

    added form validation in signup page

--- src/pages/LoginSignup/Signup.jsx
+++ src/pages/LoginSignup/Signup.jsx
@@ -17,15 +17,38 @@ const initialValues={
 }
 const Signup = () => {
   const [user,setUser]=useState(initialValues)
-  const onValueChange=(e)=>{
-    setUser({...user,[e.target.id]:e.target.value})
-    console.log(user)
-}
+  const [errors,setErrors]=useState({});
 
-const addUserDetails=async()=>{
-  await addUser(user);
+  const onValueChange = (e) => {
+    const { id, value } = e.target;
+    if (id === 'phno' && value.length !== 10) {
+      setErrors({ ...errors, [id]: 'Phone number must be 10 digits' });
+    } else if (id === 'email' && !value.includes('@')) {
+      setErrors({ ...errors, [id]: 'Email must contain @ symbol' });
+    } else {
+      setErrors({ ...errors, [id]: '' });
+      setUser({ ...user, [id]: value }); // Only update user if input is valid
+    }
+  };
+  
 
-}
+  const addUserDetails=async()=>{
+    if(validateForm()) {
+      await addUser(user);
+    }
+  }
+
+  const validateForm = () => {
+    let formIsValid = true;
+    for (const key in user) {
+      if (!user[key]) {
+        setErrors({...errors, [key]: `${key} is required`});
+        formIsValid = false;
+      }
+    }
+    return formIsValid;
+  };
+
   return (
     <div className="signup">
     <div className='container'>
@@ -37,23 +60,28 @@ const addUserDetails=async()=>{
           <formcontrol class="input">
               <img src={user_icon} alt="Name"/>
               <input type="text" onChange={(e)=> onValueChange(e)} placeholder='Name' id='name'/>
+              <span className="error">{errors['name']}</span>
           </formcontrol>
           
           <formcontrol class="input">
               <img src={email_icon} alt="" />
               <input type="email" onChange={(e)=> onValueChange(e)} placeholder='Email Id' id='email'/>
+              <span className="error">{errors['email']}</span>
           </formcontrol>
           <formcontrol class="input">
               <img src={email_icon} alt="" />
               <input type="number" onChange={(e)=> onValueChange(e)} placeholder='Phone no' id='phno'/>
+              <span className="error">{errors['phno']}</span>
           </formcontrol >
           <formcontrol class="input">
               <img src={email_icon} alt="" />
               <input type="text" onChange={(e)=> onValueChange(e)} placeholder='username' id='username'/>
+              <span className="error">{errors['username']}</span>
           </formcontrol >
           <formcontrol class="input">
               <img src={password_icon} alt="" />
               <input type="password" placeholder='Password' id='password' autoComplete='current-password' onChange={(e)=> onValueChange(e)}/>
+              <span className="error">{errors['password']}</span>
           </formcontrol>
       </div>
           <formcontrol>
