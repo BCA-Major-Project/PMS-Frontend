af5c7e8dc63aff2ac495170ae9860da59e113e96
baishali <roybaishali07@gmail.com>
2024-05-16 12:05:31 +0530

    minor changes

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -5,7 +5,7 @@ function ProjectCard() {
     return (
       <div className='dabba'>
         <p className='projName'>Our Project</p>
-        <p className='date'>Budbar May 01 2024</p>
+        <p className='date'>Wed May 01 2024</p>
         <hr className='underline'/>
         <div className='team'>
             <Member />          
