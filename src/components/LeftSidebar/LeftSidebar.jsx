3a2599f2f869a43eb486a9ca370af010bc761e71
baishali <roybaishali07@gmail.com>
2024-05-16 15:08:44 +0530

    added leftblock navigation

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,25 +1,31 @@
-import { useState } from 'react';
-import './LeftSidebar.css'
+import React from 'react';
+import './LeftSidebar.css';
 
-const username = localStorage.getItem("uname");
+const username = localStorage.getItem('uname');
 
-console.log(username)
-function LeftBlock() {
-    return (
-        <div className='left common'>
-        <div className='profile'>
-          <div className='dp'>
-            <img src="https://images.unsplash.com/photo-1603775020644-eb8decd79994?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />            
-          </div>
-          <div className='name'>{username}</div>
+function LeftBlock({ onAddProjectClick, onDashboardClick }) {
+  return (
+    <div className='left common'>
+      <div className='profile'>
+        <div className='dp'>
+          <img
+            src='https://images.unsplash.com/photo-1603775020644-eb8decd79994?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt=''
+          />
         </div>
-        <div className='btn'>
-          <div className='dashboard'>Dashboard</div>
-          <div className='project'>Add Project</div>
-        </div>
-        <div></div>
+        <div className='name'>{username}</div>
       </div>
-    )
-  }
-  
-  export default LeftBlock
+      <div className='btn'>
+        <div className='dashboard' onClick={onDashboardClick}>
+          Dashboard
+        </div>
+        <div className='project' onClick={onAddProjectClick}>
+          Add Project
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default LeftBlock;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,38 @@
-import { useState } from 'react'
-import './Home.css'
-import LeftBlock from '../../components/LeftSidebar/LeftSidebar.jsx'
-import RightBlock from '../../components/RIghtSidebar/RightSidebar.jsx'
-import TopNav from '../../components/Navbar/Navbar.jsx'
-import Dashboard from '../../components/Dashboard/Dashboard.jsx'
-import AddProject from '../../components/AddProject/AddProject.jsx'
+import React, { useState } from 'react';
+import './Home.css';
+import LeftBlock from '../../components/LeftSidebar/LeftSidebar';
+import RightBlock from '../../components/RIghtSidebar/RightSidebar';
+import TopNav from '../../components/Navbar/Navbar';
+import Dashboard from '../../components/Dashboard/Dashboard';
+import AddProject from '../../components/AddProject/AddProject';
 
 function Home() {
+  const [currentComponent, setCurrentComponent] = useState('dashboard'); 
+
+  const handleAddProjectClick = () => {
+    setCurrentComponent('addProject');
+  };
+
+  const handleDashboardClick = () => {
+    setCurrentComponent('dashboard');
+  };
+
   return (
-    
-      <div className='outer-container'>
-        <LeftBlock />
-        <div className='center common'>
-          <TopNav />
-          <div className='bottom'>
-            <Dashboard />
-          </div>
+    <div className='outer-container'>
+      <LeftBlock
+        onAddProjectClick={handleAddProjectClick}
+        onDashboardClick={handleDashboardClick}
+      />
+      <div className='center common'>
+        <TopNav />
+        <div className='bottom'>
+          
+          {currentComponent === 'dashboard' ? <Dashboard /> : <AddProject />}
         </div>
-        <RightBlock />
-      </div>    
-  )
+      </div>
+      <RightBlock />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
