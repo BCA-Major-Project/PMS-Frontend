b8e281f3e72db8d4245d19f3f583317da629db92
Sneha <doyelsd123@gmail.com>
2024-05-20 11:39:56 +0530

    landing page

--- src/pages/LoginSignup/Landing.jsx
+++ src/pages/LoginSignup/Landing.jsx
@@ -1,33 +1,114 @@
 import React from 'react';
 import './Landing.css';
+import  project  from '../assets/addproject.png';
 import project_img from '../assets/project_img.png';
+import filter from '../assets/filter.png';
+import comment from '../assets/comment.png';
 import { Link } from 'react-router-dom';
+import { addProject } from '../../service/api';
 
 const Landing = () => {
   return (
-    <div className="landing">
-      <nav className="navbar">
-        <ul className="navbar__nav">
-          <li className="navbar__nav-item">
-            <Link to="/adminlogin"><h3>Admin Login</h3></Link>
-          </li>
-          <li className="navbar__nav-item">
-            <Link to="/home"><h3>User Login/Sign Up</h3></Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="content-wrapper">
-        <div className="intro-container">
-          <h1>Project Management System</h1>
-          <p>
-          A project management system is like a conductor orchestrating a symphony, ensuring all tasks harmonize towards a successful project completion. It's the digital backbone that organizes, tracks, and streamlines every aspect of a project, from inception to delivery.
-          </p>
-        </div>
-        <div className="image-container">
-          <img src={project_img} alt="Project Management System" />
+    <div className='sper-landing'>
+    <div className='main-landing'>
+      <div className="landing">
+        <nav className="navbar">
+          <ul className="navbar__nav">
+            <li className="navbar__nav-item">
+             <h3>ProjectPulse</h3>
+            </li>
+            {/* <li className="navbar__nav-item">
+              <Link to="/home"><h3>User Login/Sign Up</h3></Link>
+            </li> */}
+          </ul>
+        </nav>
+        <div className="content-wrapper">
+          <div className="intro-container">
+            <h2>Project Management <br/>Made Easy</h2>
+            <p>
+              A project management system is like a conductor orchestrating a symphony, ensuring all tasks harmonize towards a successful project completion. It's the digital backbone that organizes, tracks, and streamlines every aspect of a project, from inception to delivery.
+            </p>
+          </div>
+          <div className="image-container">
+            <img src={project_img} alt="Project Management System" />
+          </div>
         </div>
       </div>
+      <div className='button-class'>
+        <Link to="/login"><button className='btn1'>User Login</button></Link>
+        <Link to="/adminlogin"><button className='btn2'>Admin Login</button></Link>
+      </div>
     </div>
+
+
+
+   {/* second section */}
+    <div className='second-section'>
+    <div className="wrapper">
+    <div className="center-line">
+      
+    </div>
+    <div className="row row-1">
+      <section >
+        <img src={project} alt='addproject'/>
+      </section>
+    </div>
+    <div className="row row-2">
+      <section>
+        <i className="icon fas fa-star"></i>
+        <div className="details">
+          <span className="title">Create your own project</span>
+          
+        </div>
+        <p>Give your project name and you can assign the project to all user</p>
+        <div className="bottom">
+          <a href="#">Read more</a>
+         
+        </div>
+      </section>
+    </div>
+    <div className="row row-1">
+      <section>
+      <img src={comment} alt='addproject'/>
+      </section>
+    </div>
+    <div className="row row-2">
+      <section>
+        <i className="icon fas fa-globe"></i>
+        <div className="details">
+          <span className="title">Comment on projects</span>
+        </div>
+        <p>Give your opinion on every project and see others</p>
+        <div className="bottom">
+          <a href="#">Read more</a>
+         
+        </div>
+      </section>
+    </div>
+    <div className="row row-1">
+      <section>
+      <img src={filter} alt='addproject'/>
+        
+      </section>
+    </div>
+    <div className="row row-2">
+      <section>
+      <i className="icon fas fa-paper-plane"></i>
+        <div className="details">
+          <span className="title">Filter your project</span>
+          
+        </div>
+        <p>filter you project as per your requirements.</p>
+        <div className="bottom">
+          <a >Read more</a>
+   
+        </div>
+      </section>
+    </div>
+  </div>
+      </div>
+    </div>
+    
   );
 };
 
