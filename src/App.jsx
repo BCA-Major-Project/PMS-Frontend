35940b52d3d9347ce15f04a6bebb2a4a58322b77
Protik21 <protikb2003@gmail.com>
2024-05-20 16:12:38 +0530

    Added Protection for the all pages

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,23 @@ import EditUser from './pages/Admin/EditUser';
 import Home from './pages/Home/Home';
 import EditProject from './pages/Admin/EditProject';
 // import Project from './pages/Project/Project';
+import Protected from './components/Protected/Protected'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Landing/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/forgotpwd" element={<Forgotpwd/>} />
-          <Route path="/otp" element={<OTPGenerate/>} />
-          <Route path="/userdetails" element={<Userdetails/>} />
-          <Route path="/projectdetails/:uid" element={<ProjectDetails/>} />
-          <Route path="/adminlogin" element={<Adminlogin/>} />
-          <Route path="/adminhome" element={<AdminHome/>} />
-          <Route path="/edit/:id" element={<EditUser/>} />
-          <Route path="/editproject/:id" element={<EditProject/>} />
-          <Route path="/home" element={<Home/>} />
+          <Route path="/signup" element={<Protected Component={Signup}/>} />
+          <Route path="/login" element={<Protected Component={Login}/>} />
+          <Route path="/forgotpwd" element={<Protected Component={Forgotpwd}/>} />
+          <Route path="/otp" element={<Protected Component={OTPGenerate}/>} />
+          <Route path="/userdetails" element={<Protected Component={Userdetails}/>} />
+          <Route path="/projectdetails/:uid" element={<Protected Component={ProjectDetails}/>} />
+          <Route path="/adminlogin" element={<Protected Component={Adminlogin}/>} />
+          <Route path="/adminhome" element={<Protected Component={AdminHome}/>} />
+          <Route path="/edit/:id" element={<Protected Component={EditUser}/>} />
+          <Route path="/editproject/:id" element={<Protected Component={EditProject}/>} />
+          <Route path="/home" element={<Protected Component={Home}/>} />
           {/* <Route path="/project" element={<Project/>} /> */}
       </Routes>
     </BrowserRouter>
