21db45727f8db236b44e0f7f3e2bf7b8e8f9ffc9
Protik21 <protikb2003@gmail.com>
2024-05-21 10:16:22 +0530

    delete isloggedin key data from navbar.jsx where isLoggedIn key was storing data to the localstorage so when we click on the logout button the data will be deleted from the localstorage so that we protect route of the home page with @doyelsd123

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,34 @@
+// import React from 'react';
+// import { setUserOffline } from '../../service/api';
+// import './Navbar.css';
+
+// function TopNav() {
+//   const handleLogout = () => {
+//     const userId = JSON.parse(localStorage.getItem('user'))?.id;
+//     if (userId) {
+//       setUserOffline(userId);
+//     }
+//     localStorage.removeItem('user');
+//     window.location.href = '/';
+//   };
+
+//   return (
+//     <div className='top'>
+//       <div className='logo'>
+//         <img
+//           src='https://static.vecteezy.com/system/resources/previews/024/553/853/non_2x/colorful-eagle-head-logo-pop-art-style-eagle-face-sticker-pastel-cute-colors-ai-generated-png.png'
+//           alt=''
+//         />
+//       </div>
+//       <div className='logout' onClick={handleLogout}>
+//         Logout
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default TopNav;
+
 import React from 'react';
 import { setUserOffline } from '../../service/api';
 import './Navbar.css';
@@ -9,6 +40,7 @@ function TopNav() {
       setUserOffline(userId);
     }
     localStorage.removeItem('user');
+    localStorage.removeItem('isLoggedIn');
     window.location.href = '/';
   };
 
