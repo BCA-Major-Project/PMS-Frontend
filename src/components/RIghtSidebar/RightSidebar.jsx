e808b272bfe83cfca6f5fdade17eadaa83890bee
Anirban Dutta <duttashaan102@gmail.com>
2024-05-23 19:01:18 +0530

    use user components effectively

--- src/components/RIghtSidebar/RightSidebar.jsx
+++ src/components/RIghtSidebar/RightSidebar.jsx
@@ -1,13 +1,32 @@
 import './RightSidebar.css'
+import { useState, useEffect } from 'react';
+import { getPublicUsers } from '../../service/api.js';
 import Users from '../Users/Users'
 
-function RightBlock() {
+const RightBlock = () => {
+    const [users, setUsers] = useState([]);
+    const getUsersDetails = async () => {
+        try {
+            const response = await getPublicUsers();
+            const updatedUsers = response.data.map(user => ({
+            ...user,
+            }));
+            setUsers(updatedUsers);
+        } catch (error) {
+            console.error('Error fetching user details:', error);
+        }
+    };
+    useEffect(()=>{
+        getUsersDetails();
+    },[])
     return (
         <div className='right common'>
             <div className='heading'>All Users</div>
-            <Users/>
+            {users.map((user)=>(
+                <Users user={user}/>
+            ))}
         </div>
     )
-  }
+}
 
-  export default RightBlock
+export default RightBlock
